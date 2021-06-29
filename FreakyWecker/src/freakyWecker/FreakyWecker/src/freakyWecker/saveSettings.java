493a89f95e085ce32ef3275499dbd9e68182532b
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2021-06-30 00:48:41 +0200

    Add files via upload
    unused functions deleted

---
+++ FreakyWecker/src/freakyWecker/FreakyWecker/src/freakyWecker/saveSettings.java
@@ -0,0 +1,59 @@
+package freakyWecker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class saveSettings {
+	//private static long startTime;
+	//times of the alarm clocks
+	private static List<Long> endTimes;
+	//names of the alerm clocks
+	private static List<String> nameOfWecker;
+	
+	//constructor
+	public saveSettings()
+	{
+	
+		//long startTime = (long)0;
+		endTimes=new ArrayList<Long>();
+		 nameOfWecker = new ArrayList<String>();
+		}
+	
+	
+	
+	public long getEndTime(int value) {
+		return endTimes.get(value);
+	}
+	
+	public void setEndTime(long endTime) {
+		endTimes.add(endTime);
+		//System.out.println("long "+endTime+" wurde hinzugefügt.");
+	}
+	
+	/*public static long getStartTime() {
+		return startTime;
+	}
+	
+	public static void setStartTime(long startTime) {
+		saveSettings.startTime = startTime;
+	}*/
+
+	public String getNameOfWecker(int i) {
+		return nameOfWecker.get(i);
+	}
+
+	public void setNameOfWecker(String nameOfWecker) {
+		saveSettings.nameOfWecker.add(nameOfWecker);
+	}
+	
+	public int sizeOfEndTimes()
+	{
+		return endTimes.size();
+	}
+	
+	public static void deleteEndTime(int i)
+	{
+		endTimes.remove(i);
+		nameOfWecker.remove(i);
+	}
+}
